ea944df47b677409abe78d7e0ffcdc7342f2560d
deveshkumar833-coder <deveshkumar833@gmail.com>
2025-09-14 21:31:03 +0530

    13 September Exercise

---
+++ src/ex_02_Java_basics/MMMain.java
@@ -0,0 +1,15 @@
+package ex_02_Java_basics;
+
+public class MMMain {
+    public static void main(String[] args) {  // Real main method
+
+    }
+
+    public static void main(String args) { // Dummy or not real main method
+
+    }
+
+    public static void main(int args) { // Dummy or not real main method
+
+    }
+}
---
+++ src/ex_02_Java_basics/MainMain.java
@@ -0,0 +1,7 @@
+package ex_02_Java_basics;
+
+public class MainMain {
+    public static void main(String[] args) {
+        System.out.println(10+3/2-1*5);
+    }
+}
