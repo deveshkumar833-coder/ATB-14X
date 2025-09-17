48cb0726605bf69214e85fcc9b559c3ac56b8738
deveshkumar833-coder <deveshkumar833@gmail.com>
2025-09-17 07:08:41 +0530

    13 September Exercise

---
+++ src/ex_02_Java_basics/Lab018_variables.java
@@ -0,0 +1,10 @@
+package ex_02_Java_basics;
+
+public class Lab018_variables {
+    public static void main(String[] args) {
+        int _=65;
+        int $=65;
+        int year=65;
+        int Long_yesr_present_123=65;
+    }
+}
