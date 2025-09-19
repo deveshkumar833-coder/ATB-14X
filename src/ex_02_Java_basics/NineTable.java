8043bd1507459d4a061132dbdc0ba88570eda33a
deveshkumar833-coder <deveshkumar833@gmail.com>
2025-09-19 06:39:54 +0530

    13 September Exercise

---
+++ src/ex_02_Java_basics/NineTable.java
@@ -0,0 +1,22 @@
+package ex_02_Java_basics;
+
+public class NineTable{
+    public static void main(String[] args) {
+        int number = 9;
+
+        System.out.printf("%d x %d = %d\n", number, 1, number * 1);
+        System.out.printf("%d x %d = %d\n", number, 2, number * 2);
+        System.out.printf("%d x %d = %d\n", number, 3, number * 3);
+        System.out.printf("%d x %d = %d\n", number, 4, number * 4);
+        System.out.printf("%d x %d = %d\n", number, 5, number * 5);
+        System.out.printf("%d x %d = %d\n", number, 6, number * 6);
+        System.out.printf("%d x %d = %d\n", number, 7, number * 7);
+        System.out.printf("%d x %d = %d\n", number, 8, number * 8);
+        System.out.printf("%d x %d = %d\n", number, 9, number * 9);
+        System.out.printf("%d x %d = %d\n", number, 10, number * 10);
+    }
+}
+
+
+
+
