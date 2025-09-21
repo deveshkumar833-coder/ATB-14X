57a130e07064b706929b83a53b575dc8f2aa63c1
deveshkumar833-coder <deveshkumar833@gmail.com>
2025-09-22 04:49:54 +0530

    20 september exercise

---
+++ src/ex_04_operators/Lab42_Interview_concat_plus.java
@@ -0,0 +1,13 @@
+package ex_04_operators;
+
+public class Lab42_Interview_concat_plus {
+    public static void main(String[] args) {
+        int a = 10;
+        int b=30;
+        System.out.println(a+b);
+
+        String First_name = "Devesh";
+        String Last_name = "Kumar";
+        System.out.println(First_name + Last_name);
+    }
+}
