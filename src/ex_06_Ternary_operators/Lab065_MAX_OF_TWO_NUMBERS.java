9051f6ce8dd0e80a8a3c5eaaf96ebf9dbddee735
deveshkumar833-coder <deveshkumar833@gmail.com>
2025-10-03 16:16:24 +0530

    20 september exercise ternary operator

---
+++ src/ex_06_Ternary_operators/Lab065_MAX_OF_TWO_NUMBERS.java
@@ -0,0 +1,10 @@
+package ex_06_Ternary_operators;
+
+public class Lab065_MAX_OF_TWO_NUMBERS {
+    public static void main(String[] args) {
+        int x=90;
+        int y=20;
+        String result ;
+        System.out.println(result = x > y ? "Max number is X": "Max number is Y");
+    }
+}
